ef6cce65de7403e736b447f622adbea57d7e908a
Park JaeHong <blogerhong@naver.com>
2025-01-06 13:34:37 +0900

    chore : Init Commit

---
+++ src/main/java/com/shineidle/tripf/TripfApplication.java
@@ -0,0 +1,13 @@
+package com.shineidle.tripf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shineidle/tripf/TripfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shineidle.tripf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
