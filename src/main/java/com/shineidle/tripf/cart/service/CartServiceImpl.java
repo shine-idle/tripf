c0a56fc968f12b38a14bffe701e0782cf70632e9
dbtmdgml98 <dbtmdgml98@naver.com>
2025-01-08 19:43:55 +0900

    add: cart 기능 관련 파일 생성

---
+++ src/main/java/com/shineidle/tripf/cart/service/CartServiceImpl.java
@@ -0,0 +1,12 @@
+package com.shineidle.tripf.cart.service;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class CartServiceImpl implements CartService {
+
+}
