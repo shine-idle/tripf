fd8f07cc98d04c6214da5306b30c8ac080e54228
Park JaeHong <blogerhong@naver.com>
2025-01-12 21:00:41 +0900

    refactor : 유저 관련 커스텀 예외처리 적용
    - 주석 추가

--- src/main/java/com/shineidle/tripf/user/controller/AccountController.java
+++ src/main/java/com/shineidle/tripf/user/controller/AccountController.java
@@ -33,7 +33,7 @@ public class AccountController {
      * @return {@link PostMessageResponseDto} 회원가입 완료 문구
      */
     @PostMapping("/signup")
-    public ResponseEntity<PostMessageResponseDto> createUser(
+    public ResponseEntity<PostMessageResponseDto> signUp(
             @RequestBody UserRequestDto dto
     ) {
         return new ResponseEntity<>(userService.createUser(dto), HttpStatus.CREATED);
--- src/main/java/com/shineidle/tripf/user/controller/UserController.java
+++ src/main/java/com/shineidle/tripf/user/controller/UserController.java
@@ -18,6 +18,11 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     private final UserService userService;
 
+    /**
+     * 권한 체크(비밀번호 체크)
+     * @param dto {@link UserRequestDto}
+     * @return 프로필 페이지로 리다이렉트
+     */
     @PostMapping("/verify-authority")
     public ResponseEntity<Void> verify(
             @RequestBody UserRequestDto dto
@@ -26,6 +31,11 @@ public class UserController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    /**
+     * 유저 조회
+     * @param userId 유저Id
+     * @return {@link UserResponseDto}
+     */
     @GetMapping("/{userId}")
     public ResponseEntity<UserResponseDto> findUser(
             @PathVariable Long userId
@@ -33,6 +43,11 @@ public class UserController {
         return new ResponseEntity<>(userService.findUser(userId), HttpStatus.OK);
     }
 
+    /**
+     * 패스워드 수정
+     * @param dto {@link PasswordUpdateRequestDto}
+     * @return {@link PostMessageResponseDto} 수정완료 문구
+     */
     @PatchMapping("/me")
     public ResponseEntity<PostMessageResponseDto> updatePassword(
             @RequestBody PasswordUpdateRequestDto dto
@@ -40,6 +55,11 @@ public class UserController {
         return new ResponseEntity<>(userService.updatePassword(dto), HttpStatus.OK);
     }
 
+    /**
+     * 이름 수정
+     * @param dto {@link UsernameUpdateRequestDto}
+     * @return {@link PostMessageResponseDto} 수정완료 문구
+     */
     @PutMapping("/me")
     public ResponseEntity<PostMessageResponseDto> updateName(
             @RequestBody UsernameUpdateRequestDto dto
@@ -47,6 +67,11 @@ public class UserController {
         return new ResponseEntity<>(userService.updateName(dto), HttpStatus.OK);
     }
 
+    /**
+     * 회원 탈퇴
+     * @param dto {@link UserRequestDto}
+     * @return {@link PostMessageResponseDto} 탈퇴완료 문구
+     */
     @PatchMapping("/deactivate")
     public ResponseEntity<PostMessageResponseDto> deleteUser(
             @RequestBody UserRequestDto dto
