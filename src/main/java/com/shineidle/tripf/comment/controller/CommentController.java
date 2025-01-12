cb7b83fb08a11b4f019a33c08f67522f70fbd833
shine <112529297+chews26@users.noreply.github.com>
2025-01-12 19:22:08 +0900

    add : 댓글 CRUD 기능 추가, 주석 수정, 기타 코드 수정

--- src/main/java/com/shineidle/tripf/comment/controller/CommentController.java
+++ src/main/java/com/shineidle/tripf/comment/controller/CommentController.java
@@ -1,11 +1,70 @@
 package com.shineidle.tripf.comment.controller;
 
+import com.shineidle.tripf.comment.dto.CommentRequestDto;
+import com.shineidle.tripf.comment.dto.CommentResponseDto;
+import com.shineidle.tripf.comment.service.CommentService;
+import com.shineidle.tripf.common.message.dto.PostMessageResponseDto;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/feeds/{feedId}/comments")
 public class CommentController {
+
+    private final CommentService commentService;
+
+    /**
+     * 댓글 작성
+     */
+    @PostMapping
+    public ResponseEntity<CommentResponseDto> createComment(
+            @PathVariable Long feedId,
+            @Valid @RequestBody CommentRequestDto commentRequestDto
+            ) {
+        CommentResponseDto commentResponseDto = commentService.createComment(feedId, commentRequestDto);
+        return new ResponseEntity<>(commentResponseDto, HttpStatus.CREATED);
+    }
+
+    /**
+     * 댓글 수정
+     */
+    @PutMapping("/{commentId}")
+    public ResponseEntity<CommentResponseDto> updateComment(
+            @PathVariable Long feedId,
+            @PathVariable Long commentId,
+            @Valid @RequestBody CommentRequestDto commentRequestDto
+    ) {
+        CommentResponseDto commentResponseDto = commentService.updateComment(feedId, commentId, commentRequestDto);
+        return new ResponseEntity<>(commentResponseDto, HttpStatus.OK);
+    }
+
+    /**
+     * 댓글 조회
+     */
+    @GetMapping
+    public ResponseEntity<List<CommentResponseDto>> findAllComment(
+            @PathVariable Long feedId
+    ) {
+        List<CommentResponseDto> commentResponseDto = commentService.findAllComment(feedId);
+        return new ResponseEntity<>(commentResponseDto, HttpStatus.OK);
+    }
+
+    /**
+     * 댓글 삭제
+     */
+    @DeleteMapping("/{commentId}")
+    public ResponseEntity<PostMessageResponseDto> deleteComment(
+            @PathVariable Long feedId,
+            @PathVariable Long commentId
+    ) {
+        PostMessageResponseDto postMessageResponseDto = commentService.deleteComment(feedId,commentId);
+        return new ResponseEntity<>(postMessageResponseDto, HttpStatus.OK);
+    }
+
 }
