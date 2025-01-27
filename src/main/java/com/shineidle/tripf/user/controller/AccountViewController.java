963f3d5a5dadb6e5ab574ec4fa7d6f725c7a9642
Park JaeHong <blogerhong@naver.com>
2025-01-27 20:19:37 +0900

    update : 유저 관련 프론트 작업
    - 회원가입, 로그인 파트

--- src/main/java/com/shineidle/tripf/user/controller/AccountViewController.java
+++ src/main/java/com/shineidle/tripf/user/controller/AccountViewController.java
@@ -1,10 +1,15 @@
 package com.shineidle.tripf.user.controller;
 
+import com.shineidle.tripf.common.exception.GlobalException;
+import com.shineidle.tripf.user.dto.UserRequestDto;
 import com.shineidle.tripf.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -14,18 +19,49 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AccountViewController {
     private final UserService userService;
 
-    @GetMapping("/login")
-    public String loginPage() {
-        return "user/login";
-    }
-
     @GetMapping("/signup")
     public String signUpPage() {
         return "user/signup"; // signup.html 뷰 반환
     }
 
+    @PostMapping("/signup")
+    public String signUp(
+            @ModelAttribute UserRequestDto dto,
+            Model model
+    ) {
+        try {
+            userService.createUser(dto);
+            model.addAttribute("message", "회원가입이 성공적으로 완료되었습니다.");
+            return "user/signup-success";
+        } catch (GlobalException e) {
+            model.addAttribute("error", e.getMessage());
+            return "user/signup";
+        }
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "user/login";
+    }
+
+    @PostMapping("/login")
+    public String login(
+            @ModelAttribute UserRequestDto dto,
+            Model model
+    ) {
+        try {
+            userService.login(dto);
+            model.addAttribute("message", "로그인 성공!");
+            return "home/home-logged-in";
+        } catch (GlobalException e) {
+            model.addAttribute("error", e.getMessage());
+            return "user/login";
+        }
+    }
+
     @GetMapping("/logout")
     public String logoutPage() {
+
         return "user/logout"; // logout.html 뷰 반환
     }
 }
