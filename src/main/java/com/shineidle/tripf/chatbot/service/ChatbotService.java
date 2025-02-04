719f323fc9369cfc5371d560966831b6cd45ff08
suki <astre15@naver.com>
2025-02-05 00:18:23 +0900

    Refactor: service 주석 추가
    1. 챗봇
    2. 카트

--- src/main/java/com/shineidle/tripf/chatbot/service/ChatbotService.java
+++ src/main/java/com/shineidle/tripf/chatbot/service/ChatbotService.java
@@ -7,9 +7,18 @@ import com.shineidle.tripf.chatbot.dto.ChatbotResponseDto;
 import java.util.List;
 //TODO : javadoc
 public interface ChatbotService {
+    /**
+     * 챗봇 응답을 생성
+     */
     ChatbotResponseDto createChatbotResponse(ChatbotRequestDto chatbotRequestDto);
 
+    /**
+     *챗봇 질문 목록 조회
+     */
     List<ChatbotQuestionsResponseDto> findAllChatbotQuestion();
 
+    /**
+     *사용자 대화 기록 조회
+     */
     List<ChatbotResponseDto> findConversationLogs();
 }
