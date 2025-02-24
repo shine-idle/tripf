8698d60686f4430362fff9809cb359fe014fc6cf
Park JaeHong <blogerhong@naver.com>
2025-02-24 21:36:49 +0900

    refactor : JwtProvider, JwtAuthFilter 리팩토링

--- src/main/java/com/shineidle/tripf/global/security/filter/JwtAuthFilter.java
+++ src/main/java/com/shineidle/tripf/global/security/filter/JwtAuthFilter.java
@@ -1,5 +1,7 @@
 package com.shineidle.tripf.global.security.filter;
 
+import com.shineidle.tripf.global.common.exception.GlobalException;
+import com.shineidle.tripf.global.common.exception.type.UserErrorCode;
 import com.shineidle.tripf.global.common.util.auth.AuthenticationScheme;
 import com.shineidle.tripf.global.common.util.provider.JwtProvider;
 import jakarta.servlet.FilterChain;
@@ -22,11 +24,9 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthFilter extends OncePerRequestFilter {
-
     private final JwtProvider jwtProvider;
 
     private final UserDetailsService userDetailsService;
-
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -41,12 +41,12 @@ public class JwtAuthFilter extends OncePerRequestFilter {
      */
     private void authenticate(HttpServletRequest request) {
         String token = this.getTokenFromRequest(request);
-        if (!jwtProvider.validToken(token)) {
+
+        if (jwtProvider.isInvalidToken(token)) {
             return;
         }
 
         String username = this.jwtProvider.getUsername(token);
-
         UserDetails userDetails = userDetailsService.loadUserByUsername(username);
 
         this.setAuthentication(request, userDetails);
@@ -62,12 +62,11 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         final String bearerToken = request.getHeader(HttpHeaders.AUTHORIZATION);
         final String headerPrefix = AuthenticationScheme.generateType(AuthenticationScheme.BEARER);
 
-        boolean tokenExist = StringUtils.hasText(bearerToken) && bearerToken.startsWith(headerPrefix);
-        if (tokenExist) {
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(headerPrefix)) {
             return bearerToken.substring(headerPrefix.length());
         }
 
-        return null;
+        throw new GlobalException(UserErrorCode.HEADER_TOKEN_NOT_FOUND);
     }
 
     /**
