fd313e6888b7a06c2e903776593a787ccf4a6fe1
Park JaeHong <blogerhong@naver.com>
2025-03-17 03:27:35 +0900

    refactor : 마이페이지 리팩토링
    - 프로필 수정 모달 작업

--- src/main/java/com/shineidle/tripf/domain/user/controller/UserViewController.java
+++ src/main/java/com/shineidle/tripf/domain/user/controller/UserViewController.java
@@ -50,13 +50,13 @@ public class UserViewController {
     /**
      * 사용자가 입력한 인증 정보를 처리, 프로필 페이지로 리다이렉션
      *
-     * @param dto 사용자 인증 요청 데이터
-     * @return 프로필 페이지로 리다이렌션
+     * @param password 비밀번호
+     * @return 프로필 페이지로 리다이렉션
      */
     @PostMapping("/verify")
-    public String verify(@ModelAttribute UserRequestDto dto) {
-        userService.verify(dto);
-        return "redirect:/users/profile";
+    public String verify(@RequestBody UserRequestDto password) {
+        userService.verify(password);
+        return "redirect:/mypage";
     }
 
     /**
